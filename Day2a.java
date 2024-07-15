18cda54c1605f1c1e9e595243fd1123341da7651
unknown <narulaharpreet90@gmail.com>
2024-07-16 01:33:46 +0530

    first

---
+++ Day2a.java
@@ -0,0 +1,97 @@
+class Day2a{
+  public static void main(String[]args){
+    int i=1;
+    while (i<=10){
+      System.out.println();
+    }
+  }
+}
+//
+
+//
+
+//class Day2a{
+  //  public static void main(String[]args){
+       // int n=10;
+        //for(int i=1;i<=n;i++){
+            //System.out.println("*");
+      //  }
+   // }
+//}
+
+//class Day2a {
+   // public static void main(String[]args){
+      //  int n=5;
+       //  for(int i=1;i<=n;i++){
+            //for(int j=1;j<=n;j++){
+             //   System.out.println("*");
+               // System.out.print("");
+       //  }
+       // }
+    //}
+//
+//public class Day2a {
+//public static void main(String[]args){
+    //int n=4;
+    // for(int i=1;i<=n;i++){
+        //for(int j=1;j<=n;j++){
+            //System.out.print("*");
+           //// System.out.println();
+   //  }
+   // }
+//}
+
+//public class Day2a {
+   // public static void main(String[]args){
+       // int n=4;
+        // for(int i=1;i<=n;i++){
+           // for(int k=1;k<=(n-i);k++)
+           // System.out.print("  ");
+           // for(int j=1;j<=i;j++)
+             //   System.out.print("*");
+           //     System.out.println();
+       //  }
+      //  }
+    //}
+
+//public class Day2a {
+    //public static void main(String[]args){
+       // int n=4;
+        //for(int i=n;i>=1;i--){
+           // for(int j=1;j<=i;j++)
+           // System.out.print("*");
+          //  System.out.println();
+      //  }
+    //}
+//}
+
+//public class Day2a {
+   // public static void main(String[]args){
+       // int n=4;
+        //for(int i=n;i>=i;i--){
+           // for(int k=1;k<=(n-1);k++)
+           // System.out.print("   ");
+           // for(int j=1;j<=i;j++)
+          //  System.out.print("*");
+          //  System.out.println();
+      //  }
+   // }
+//
+
+//public class Day2a {
+    //public static void main(String[]args){
+      //  int n=4;
+       // for(int i=1;i<=n;i++){
+         //   for(int k=1;k<=(n-1);k++){
+                
+          //  for(int j=1;j<=n;j++){
+           //     if(i==1)
+           // System.out.print("* ");
+           // System.out.println();
+       // }
+   // }
+//}
+    
+    
+
+
---
+++ Day2c.java
@@ -0,0 +1,8 @@
+public class Day2c {
+    public static void main(String[]args) {
+        int i=1;
+        while(i<=10) {
+        System.out.println(); 
+        }
+    }
+}
